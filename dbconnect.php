730de3d2de7cded89c44cddb681e69b2ab0eeccf
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-15 21:34:47 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php  
-$mysqli = new mysqli("https://databases.000webhost.com/", "id3115110_root","kmutnb","id3115110_line_bot_php");  
+$mysqli = new mysqli("https://databases.000webhost.com/db_structure.php?server=1&db=id3115110_line_bot_php&token=177c771fb2335b060f60956b5d2ee205", "id3115110_root","kmutnb","id3115110_line_bot_php");  
 /* check connection */
 if ($mysqli->connect_errno) {  
     printf("Connect failed: %s\n", $mysqli->connect_error);  
