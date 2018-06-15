7f93fa96cef8a3b61be6b5d959d8bbafe87fbe02
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-15 17:25:46 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php  
-$mysqli = new mysqli("localhost", "root","","arduino");  
+$mysqli = new mysqli("localhost", "root","12345678","arduino");  
 /* check connection */
 if ($mysqli->connect_errno) {  
     printf("Connect failed: %s\n", $mysqli->connect_error);  
