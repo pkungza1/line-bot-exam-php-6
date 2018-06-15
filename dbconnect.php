402e9e8aeb90a14d327cc398ba805a6d4f33a295
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-15 17:35:58 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,5 @@
 <?php  
 $mysqli = new mysqli("localhost", "root","","arduino");
 $objConnect = mysqli_connect($servername,$username,$password,$dbname) or die("Error Connect to Database");
-$SQL = "SELECT * FROM temp";
-$objQuery = mysqli_query($objConnect,$SQL) or die ("Error Query [".$strSQL."]");
-/* check connection */
-if ($mysqli->connect_errno) {  
-    printf("Connect failed: %s\n", $mysqli->connect_error);  
-    exit();  
-}  
-if(!$mysqli->set_charset("utf8")) {  
-    printf("Error loading character set utf8: %s\n", $mysqli->error);  
-    exit();  
+
 }
