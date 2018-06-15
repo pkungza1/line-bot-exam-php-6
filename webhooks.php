7e89ac629bfb22796e3ef49e9fbd931c13c31932
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-15 11:15:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -48,24 +48,17 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
  
-// เชื่อมต่อกับ LINE Messaging API
+ 
 $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
 $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
  
-// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
-$content = file_get_contents('php://input');
- 
-// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
-$events = json_decode($content, true);
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
-}
-// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder(json_encode($events));
- 
-//l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$textMessageBuilder);
+// userId 
+$userId = 'U299a312eaf887dbd304427744e45778a ';
+// ทดสอบส่ง push ข้อความอย่างง่าย
+$textPushMessage = 'สวัสดีครับ';                
+$messageData = new TextMessageBuilder($textPushMessage);        
+             
+$response = $bot->pushMessage($userId,$messageData);
 if ($response->isSucceeded()) {
     echo 'Succeeded!';
     return;
