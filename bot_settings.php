83d87776ef1c5935f989dfea8c7d72fe80c72742
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-09-07 08:43:24 +0700

    Add files via upload

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1585854926');
+define('LINE_MESSAGE_CHANNEL_SECRET','972eda038e3b3d879d3f3edbb9c314ed');
+define('LINE_MESSAGE_ACCESS_TOKEN','sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=');
+?>
---
+++ dbconnect.php
@@ -0,0 +1,19 @@
+<?php
+header("content-type:application/json");
+//$jsondata = file_get_contents("https://commember14.000webhostapp.com/show1.php");
+
+//echo $jsondata
+
+// Read JSON Decode //
+    $jsonCode = file_get_contents("https://commember14.000webhostapp.com/show1.php");
+	$jsonDecode = json_decode($jsonCode, true);
+
+	foreach ($jsonDecode as $objResult)
+	{
+		echo $objResult["id"]." ";
+		echo $objResult["temp"]." ";
+		echo $objResult["humidity"]." ";
+		echo $objResult["date"]." ";
+	}
+
+?>
